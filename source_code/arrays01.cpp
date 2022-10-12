aaad39a312b19b26da5e615e3e4b874a64481a32
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-12 21:29:08 +0300

    Create arrays01.cpp

---
+++ source_code/arrays01.cpp
@@ -0,0 +1,24 @@
+// arrays01.cpp
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	
+	/*
+		string pizzas[4];
+
+		pizzas[0] = "Peperoni";
+		pizzas[1] = "Margarita";
+		pizzas[2] = "Special";
+		pizzas[3] = "Vezouvios";
+	*/
+	
+	string pizzas[4] = { "Peperoni", "Margarita", "Special", "Vezouvios" };
+	
+	cout << "My favourite pizza is " << pizzas[0] << endl;
+	
+	return 0;
+}
