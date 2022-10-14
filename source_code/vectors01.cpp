f6207c75d13101e102069d26cd645afa3a3f02a7
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-14 23:37:23 +0300

    Create vectors01.cpp

---
+++ source_code/vectors01.cpp
@@ -0,0 +1,23 @@
+// vectors01.cpp
+//
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> a;
+    
+    a.push_back(1);
+    a.push_back(2);
+    a.push_back(3);
+    a.push_back(4);
+    a.push_back(5);
+
+    for (vector<int>::iterator itr = a.begin(); itr != a.end(); ++itr)
+        cout << *itr << endl;
+
+    return 0;
+}
