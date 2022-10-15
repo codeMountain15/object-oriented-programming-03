bcf3019e4eef2498a61e0fde52a2402b1e025759
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-15 14:41:48 +0300

    Create arrays04.cpp

---
+++ source_code/arrays04.cpp
@@ -0,0 +1,19 @@
+// arrays04.cpp
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	char client_name[] = "Mitsos";	// double quotes for strings
+	cout << client_name << endl;
+
+	client_name[0] = 'K';			// single quotes for characters
+	cout << client_name << endl;
+
+
+	return 0;
+
+}
