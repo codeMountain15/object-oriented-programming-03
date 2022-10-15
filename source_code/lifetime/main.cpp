8104a7dc2bdf6225f2b857a2d452c1796e940cb6
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-15 20:41:57 +0300

    Update main.cpp

--- source_code/lifetime/main.cpp
+++ source_code/lifetime/main.cpp
@@ -8,14 +8,9 @@ int func();
 
 int main() {
 
-    func();
     cout << "Value of num from main() - 1st call: " << func() << "\n\n";
-
-    func();
     cout << "Value of num from main() - 2nd call: " << func() << "\n\n";
-
-    func();
     cout << "Value of num from main() - 3rd call: " << func() << "\n\n";
-    
+
     return 0;
 }
