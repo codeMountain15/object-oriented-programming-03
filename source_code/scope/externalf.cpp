334667da6fc605fc6f33da6187bd10726eef5e78
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-15 16:53:55 +0300

    Create externalf.cpp

---
+++ source_code/scope/externalf.cpp
@@ -0,0 +1,24 @@
+// externalf.cpp
+//
+
+#include <iostream>
+
+using namespace std;
+
+int my_var = 0;
+
+void f(){
+
+     int my_var;
+
+     my_var = 1;
+     cout << "print my_var from 1st externalf.cpp " << ::my_var << endl;       // prints 0
+
+     {
+        int my_var;
+        my_var = 2;
+        cout << "print my_var from internal externalf.cpp " << my_var << endl; // prints 2
+     }
+
+     cout << "print my_var from 2nd externalf.cpp " << my_var << endl;         // prints 1
+ }
