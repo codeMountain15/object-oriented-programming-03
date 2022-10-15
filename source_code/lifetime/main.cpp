fe12cb259737fba7b4dd52967dc2be132a01dfb3
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-15 19:46:17 +0300

    Create main.cpp

---
+++ source_code/lifetime/main.cpp
@@ -0,0 +1,21 @@
+// lifetime/main.cpp
+//
+
+#include <iostream> 
+using namespace std;
+
+int func();
+
+int main() {
+
+    func();
+    cout << "Value of num from main() - 1st call: " << func() << "\n\n";
+
+    func();
+    cout << "Value of num from main() - 2nd call: " << func() << "\n\n";
+
+    func();
+    cout << "Value of num from main() - 3rd call: " << func() << "\n\n";
+
+    return 0;
+}
